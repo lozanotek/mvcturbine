2fdab2dad2dbbf50551a98dc8a73634f609a19db
Javier Lozano <javier@lozanotek.com>
2010-04-28 23:04:36 -0500

    Updated the samples and binaries to the latest bits in preparation to release v2.1

--- src/Samples/Features/MultipleViewEngines/MvcApplication/Global.asax.cs
+++ src/Samples/Features/MultipleViewEngines/MvcApplication/Global.asax.cs
@@ -1,15 +1,16 @@
 namespace MvcTurbine.Samples.MultipleViewEngines {
     using ComponentModel;
-    using Unity;
     using Web;
+    using Windsor;
 
     public class MvcApplication : TurbineApplication {
         static MvcApplication() {
             // Register the IoC that you want Mvc Turbine to use!
             // Everything else is wired automatically
 
-            // For now, let's use the Castle Windsor IoC
-            ServiceLocatorManager.SetLocatorProvider(() => new UnityServiceLocator());
+            // For now, let's use the Castle Windsor IoC since it will resolve the view engine's
+            // default constructor
+            ServiceLocatorManager.SetLocatorProvider(() => new WindsorServiceLocator());
         }
     }
 }
