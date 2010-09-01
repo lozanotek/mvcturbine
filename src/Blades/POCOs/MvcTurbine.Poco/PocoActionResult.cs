dd8b6dfa6c5123219657f4a15558dff26d1fbee2
jglozano <javier@lozanotek.com>
2010-09-01 11:37:24 -0500

    Changed implementation to use Json.Net from Newtonsoft instead of the built-in pieces.

--- src/Blades/POCOs/MvcTurbine.Poco/PocoActionResult.cs
+++ src/Blades/POCOs/MvcTurbine.Poco/PocoActionResult.cs
@@ -1,5 +1,8 @@
 ﻿namespace MvcTurbine.Poco {
+	using System;
+	using System.Web;
 	using System.Web.Mvc;
+	using Newtonsoft.Json;
 
 	public class PocoActionResult : ActionResult {
 		public PocoActionResult() {
@@ -12,8 +15,18 @@
 		public object Model { get; set; }
 
 		public override void ExecuteResult(ControllerContext context) {
-			JsonResult result = new JsonResult { Data = Model, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
-			result.ExecuteResult(context);
+			if (context == null) {
+				throw new ArgumentNullException("context");
+			}
+
+			HttpResponseBase response = context.HttpContext.Response;
+			response.ContentType = "application/json";
+
+			var writer = new JsonTextWriter(response.Output) { Formatting = Formatting.None };
+			JsonSerializer serializer = JsonSerializer.Create(new JsonSerializerSettings());
+			serializer.Serialize(writer, Model);
+
+			writer.Flush();
 		}
 	}
 }
