4584fb8a3d51dc3a9e9520209901df846d87032e
Darren Cauthon <darren@cauthon.com>
2010-09-28 21:26:53 -0500

    Add short xml comment.

--- src/Engine/MvcTurbine.Web/Metadata/CreateMetadataArguments.cs
+++ src/Engine/MvcTurbine.Web/Metadata/CreateMetadataArguments.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 
 namespace MvcTurbine.Web.Metadata
 {
+    /// <summary>
+    /// The arguments that are normally passed to the CreateMetadata 
+    /// method of the DataAnnotationsModelMetadataProvider.
+    /// </summary>
     public class CreateMetadataArguments
     {
         public IEnumerable<Attribute> Attributes { get; set; }
