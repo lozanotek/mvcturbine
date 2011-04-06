43b401f2fe911a5cd42c49d711585e0ad43e7a08
Javier Lozano <javier@lozanotek.com>
2011-04-05 22:33:29 -0500

    Changed the build script to use .NET 4

--- build/build_binaries.bat
+++ build/build_binaries.bat
@@ -1,4 +1,4 @@
 RMDIR /S /Q binaries
-%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild ..\src\Engine\MvcTurbine.sln -t:Clean
-%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild ..\src\Engine\MvcTurbine.sln -p:Configuration=Release
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ..\src\Engine\MvcTurbine.sln -t:Clean
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild ..\src\Engine\MvcTurbine.sln -p:Configuration=Release
 pause
