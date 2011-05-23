8f58243f349c71666a243fe46bef505654febe21
jglozano <javier@lozanotek.com>
2011-05-22 22:15:31 -0500

    Added the v3.1 and v3.2 nuget packages as well as updates to the CoreBlades pieces.

--- build/nuget/packages.bat
+++ build/nuget/packages.bat
@@ -1,5 +1,5 @@
-nuget pack MVCTurbine/3.1/MVCTurbine.nuspec
-nuget pack MVCTurbine.Ninject/3.1/MVCTurbine.Ninject.nuspec
-nuget pack MVCTurbine.StructureMap/3.1/MVCTurbine.StructureMap.nuspec
-nuget pack MVCTurbine.Unity/3.1/MVCTurbine.Unity.nuspec
-nuget pack MVCTurbine.Windsor/3.1/MVCTurbine.Windsor.nuspec
+nuget pack MVCTurbine/3.2/MVCTurbine.nuspec
+nuget pack MVCTurbine.Ninject/3.2/MVCTurbine.Ninject.nuspec
+nuget pack MVCTurbine.StructureMap/3.2/MVCTurbine.StructureMap.nuspec
+nuget pack MVCTurbine.Unity/3.2/MVCTurbine.Unity.nuspec
+nuget pack MVCTurbine.Windsor/3.2/MVCTurbine.Windsor.nuspec
---
+++ src/Engine/MvcTurbine.Web/Blades/CoreBlades.cs
@@ -0,0 +1,64 @@
+﻿namespace MvcTurbine.Web.Blades {
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using ComponentModel;
+	using MvcTurbine.Blades;
+
+	/// <summary>
+	/// Class used to keep track of the <see cref="CoreBlade"/> types and 
+	/// instantiate them through container.
+	/// </summary>
+	public static class CoreBlades {
+		private static IDictionary<Type, Type> bladeTable = new Dictionary<Type, Type>();
+
+		/// <summary>
+		/// Adds the specified <see cref="CoreBlade"/> type to the system.
+		/// </summary>
+		/// <typeparam name="TBlade"></typeparam>
+		internal static void Track<TBlade>() where TBlade: CoreBlade {
+			bladeTable[typeof(TBlade)] = typeof(TBlade);
+		}
+
+		/// <summary>
+		/// Removes the specified <see cref="CoreBlade"/> type from the system.
+		/// </summary>
+		/// <typeparam name="TBlade"></typeparam>
+		internal static void UnTrack<TBlade>() where TBlade : CoreBlade {
+			bladeTable.Remove(typeof(TBlade));
+		}
+		
+		/// <summary>
+		/// Registers the tracked <see cref="CoreBlade"/> types with the <see cref="IServiceLocator"/>.
+		/// </summary>
+		/// <param name="locator"></param>
+		internal static void RegisterWithServiceLocator(IServiceLocator locator) {
+			if (bladeTable.Count == 0) return;
+
+			foreach (var bladeType in bladeTable.Keys) {
+				locator.Register(bladeType, bladeType);
+			}
+		}
+
+		/// <summary>
+		/// Resets the currently registered <see cref="CoreBlade"/> types.
+		/// </summary>
+		internal static void Reset() {
+			bladeTable.Clear();
+		}
+
+		/// <summary>
+		/// Gets all the <see cref="CoreBlade"/> types that were registered with the system.
+		/// </summary>
+		/// <param name="locator"></param>
+		/// <returns></returns>
+		public static BladeList GetCoreBlades(this IServiceLocator locator) {
+			if (bladeTable.Count == 0) return null;
+
+			var coreBlades = bladeTable.Values
+				.Select(bladeType => locator.Resolve(bladeType) as CoreBlade);
+
+			return new BladeList(coreBlades);
+		}
+	}
+}
