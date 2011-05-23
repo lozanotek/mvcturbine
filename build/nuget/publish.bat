3f98d2eea2ee288e0d6869a7e4f82efc8c829aff
jglozano <javier@lozanotek.com>
2011-05-22 22:21:49 -0500

    Added the publish.bat file to push out to nuget.org

---
+++ build/nuget/publish.bat
@@ -0,0 +1,5 @@
+nuget push MvcTurbine.3.2.nupkg %1
+nuget push MvcTurbine.Ninject.3.2.nupkg %1
+nuget push MvcTurbine.Unity.3.2.nupkg %1
+nuget push MvcTurbine.StructureMap.3.2.nupkg %1
+nuget push MvcTurbine.Windsor.3.2.nupkg %1
